af94dc9618a2b2b84139391d331afcda8570b520
Anu Bilegdemberel <anu.bnda@gmail.com>
2026-05-10 01:57:23 +0900

    feat(capture): add BiosignalCaptureService + Flutter MethodChannel

--- frontend/android/app/src/main/kotlin/com/littlesignals/app/MainActivity.kt
+++ frontend/android/app/src/main/kotlin/com/littlesignals/app/MainActivity.kt
@@ -1,5 +1,12 @@
 package com.littlesignals.app
 
+import com.littlesignals.app.capture.CaptureChannels
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterActivity() {
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        CaptureChannels.register(this, flutterEngine)
+    }
+}
